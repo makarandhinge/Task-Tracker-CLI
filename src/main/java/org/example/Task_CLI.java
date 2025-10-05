5d2c21f7994d473cd31342dce185a2ea67a034be
Makarand Milind Hinge <mmh2212002@gmail.com>
2025-10-05 22:27:38 +0530

    Now we started actual logic of the project
    Structure of switch case is ready. File exists is handled.

--- src/main/java/org/example/Task_CLI.java
+++ src/main/java/org/example/Task_CLI.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.example.Model.DummyModel;
@@ -14,12 +15,52 @@ public class Task_CLI {
 
     public static void main(String[] args) throws IOException {
 
-        File file = new File("products.json");
-        URL url = new URL("https://dummyjson.com/products");
-        ObjectMapper mapper = new ObjectMapper();
-        Products model = mapper.readValue(url,Products.class);
-        mapper.writerWithDefaultPrettyPrinter().writeValue(file, model);
+        File file = new File("task_tracker.json");
 
+        if(!file.exists()){
+            file.createNewFile();
+        }
+
+        String command = args[0];
+
+        switch(command.toLowerCase()){
+            case "add":
+                if(args.length < 3){
+                    System.out.println("usage: java Task_CLI add <task_name> <task_description>");
+                }else{
+                    String taskName = args[1];
+                    String taskDescription = args[2];
+                }
+                break;
+
+            case "update":
+                break;
+
+            case "delete":
+                break;
+
+            case "mark-in-progress":
+                break;
+
+            case "mark-done":
+                break;
+
+            case "list":
+                break;
+
+            case "list done":
+                break;
+
+            case "list todo":
+                break;
+
+            case "list in-progress":
+                break;
+
+            default:
+                System.out.println("Invalid command" + command);
+                System.out.println("Available commands: add, update, delete, mark-in-progress, mark-done, list, list done, list todo, list in-progress");
+        }
 
     }
 }
