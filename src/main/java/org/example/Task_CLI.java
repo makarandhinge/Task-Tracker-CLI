e70e312f2bf171287aafd1e180fc7415d39a08de
Makarand Milind Hinge <mmh2212002@gmail.com>
2025-10-05 13:53:56 +0530

    Using jackson write the existing json file

--- src/main/java/org/example/Task_CLI.java
+++ src/main/java/org/example/Task_CLI.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
@@ -14,17 +13,12 @@ public class Task_CLI {
         File file = new File("newFile.json");
 
         ObjectMapper mapper = new ObjectMapper();
-        JsonNode root = mapper.readTree(file);
-        int id = root.get("id").asInt();
-        String description = root.get("description").asText();
-        String status = root.get("status").asText();
-        int createdAt = root.get("created_at").asInt();
-        String updatedAt = root.get("updated_at").asText();
-        System.out.println("ID: " + id);
-        System.out.println("Description: " + description);
-        System.out.println("Status: " + status);
-        System.out.println("Created At: " + createdAt);
-        System.out.println("Updated At: " + updatedAt);
-
+        ObjectNode root = mapper.createObjectNode();
+        root.put("id", "1");
+        root.put("description", "description");
+        root.put("status", "status");
+        root.put("created_at", "created_at");
+        root.put("updated_at", "updated_at");
+        mapper.writeValue(file, root);
     }
 }
